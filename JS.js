3e404a3f75105fa3a72a5d7e41263e6596c70b71
Shivansh Srivastava <106915500+Shivs-7@users.noreply.github.com>
2023-07-16 15:05:12 +0530

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,4 +1,3 @@
-// Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -18,16 +17,12 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
-
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
